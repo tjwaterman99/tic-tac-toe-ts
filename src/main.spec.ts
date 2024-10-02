910aa44f4fa159e8205978bb8e948a9218abee73
Tom Waterman <tjwaterman99@gmail.com>
2024-10-02 11:45:31 +0000

    Some more wip

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,14 +1,30 @@
-import { Player, Game } from './main'
+import { Player, Game, Piece } from './main'
 
 
 describe("The game", () => {
-    let player1: Player = {name: 'Tom', symbol: 'X'}
-    let player2: Player = {name: 'Hannah', symbol: 'O'}
-    let game = new Game({
-        players: [player1, player2]
+    let player1: Player
+    let player2: Player
+    let game: Game
+    let pieces: Piece[]
+
+    beforeEach( () => {
+        player1 = {name: 'Tom', symbol: 'X'}
+        player2 = {name: 'Hannah', symbol: 'O'}
+        pieces = Array.from(Array(9).keys(), (i) => ({index: i}))
+        game = new Game({
+            players: [player1, player2],
+            pieces: pieces
+        })
+    
     })
 
-    test("Has multiple players", () => {
-        expect(game.players.length).toBeGreaterThan(0)   
+    test("Starts with multiple players", () => {
+        expect(game.players.length).toBe(2)
+    })
+
+    test("Starts with 9 pieces from 0 thru 8", () => {
+        expect(game.pieces.length).toBe(9)
+        expect(game.pieces[0].index).toBe(0)
+        expect(game.pieces[8].index).toBe(8)
     })
 })
--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,23 @@ export interface Player {
     symbol: string
 }
 
+export interface Piece {
+    index: number
+    player?: Player
+}
+
 export interface GameState {
-    players: Player[]
+    players: Player[],
+    pieces: Piece[]
 }
 
 export class Game {
     players: Player[]
+    pieces: Piece[]
 
     constructor(state: GameState) {
         this.players = state.players
+        this.pieces = state.pieces
     }
 
     echo(word: string) {
